9a2ba5c88514c7b3f80e598480c885877f05c6ca
皮蛋 <pidan@pidandeMacBook-Pro.local>
2024-04-01 18:08:33 +0800

    更新xml和页面主体

--- api/main.py
+++ api/main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='../templates')
+#模板文件夹位于当前文件的父级目录中的 templates 文件夹下
 
 @app.route("/")
-def home():
-    return "我爱你余嘉怡"
+def index():
+    return render_template('index.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    sitemap_file_path = '../sitemap.xml'  # 替换为您的站点地图文件路径
+    return send_file(sitemap_file_path, mimetype='text/xml')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
